95ff2c975285f151a751c7eeedb89dbdf92b979f
NaveenMadanapuri <139567313+NaveenMadanapuri@users.noreply.github.com>
2023-11-27 20:25:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let count=0;
+const button=document.getElementById("btn");
+const para=document.getElementById("count")
+button.addEventListener("click",function(){
+	alert(count++);
+	para.innerText++;
+})
